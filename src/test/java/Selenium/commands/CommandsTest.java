d779629169c128be9359268dddfa3502b5befc11
user2 <nika.jvelauri.1@btu.edu.ge>
2021-07-22 14:57:04 +0400

    created Second Test Case

--- src/test/java/Selenium/commands/CommandsTest.java
+++ src/test/java/Selenium/commands/CommandsTest.java
@@ -22,6 +22,19 @@ public class CommandsTest {
         WebElement element= driver.findElement(By.cssSelector("input[type=checkbox]"));
         element.click();
     }
+    @Test
+    public void inputField() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://the-internet.herokuapp.com/dynamic_controls");
+        driver.manage().window().maximize();
+        WebElement element= driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/form[2]/button"));
+        WebElement element1= driver.findElement(By.cssSelector("#input-example > input[type=text]"));
+        element.click();
+        Thread.sleep(3000);
+        element1.sendKeys("Bootcamp");
+        element1.clear();
 
+    }
 
 }
