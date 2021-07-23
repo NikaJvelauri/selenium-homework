5ec33952a8902bff8f5e27e6e6b47d8cf3ad529e
user2 <nika.jvelauri.1@btu.edu.ge>
2021-07-24 01:02:06 +0400

    made seconds test

--- src/test/java/Selenium/commands/WebElementsTest.java
+++ src/test/java/Selenium/commands/WebElementsTest.java
@@ -26,7 +26,17 @@ public class WebElementsTest {
         System.out.println("Printed Text: "+ driver.findElement(By.xpath("/html/body/div[2]/div/div/div/button[3]")).getText());
         System.out.println("Printed Text: "+ driver.findElements(By.cssSelector(".added-manually")));
         System.out.println("Printed Text: "+ driver.findElement(By.xpath("//button[contains(@class, 'manually') and text() = 'Delete']")).getText());
-
+    }
+    @Test
+    public void challengingDom(){
+        driver.get("http://the-internet.herokuapp.com/challenging_dom");
+        driver.manage().window().maximize();
+//        WebElement element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div/div[2]/table/tbody/tr[10]/td[1]"));
+        System.out.println("Lorem Value: " + driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div/div[2]/table/tbody/tr[10]/td[2-1]")).getText());
+        System.out.println("Lorem Value: " + driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div/div[2]/table/tbody/tr[10]/td[2+1]")).getText());
+        /*td[2+1] da /td[2-1] cota sacacilod chans 1 ro davamate da davakeli,
+         mara davalebashi ewera ro konkretul [2] adgilze mdgomi sityvis wina da shemdegi unda dagveprinta da yvelaze martivi es movifiqre :
+         (edit  after 5 minutes of thinking) tan vfiqrob parent-child funqcia xo ar mchirdeba da araferi mafqirdeba :d * ro gadavce an ragac egeti :D */
 
     }
 }
